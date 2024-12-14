937bb8f5c5af2ec1049fd5bd742a2e3f4bbb560c
mgn <mgn@web-control.ru>
2024-12-14 22:35:49 +0300

    Removed unused imports

--- src/main/java/maratik/budget_manager/BudgetManagerApplication.java
+++ src/main/java/maratik/budget_manager/BudgetManagerApplication.java
@@ -2,11 +2,6 @@ package maratik.budget_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class BudgetManagerApplication {
--- src/main/java/maratik/budget_manager/security/SecurityConfiguration.java
+++ src/main/java/maratik/budget_manager/security/SecurityConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
